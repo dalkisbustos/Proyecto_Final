cbb6e876c4d53c11db159b1511372f320c37d974
Dalkis Bustos Olivo <116747654+dalkisbustos@users.noreply.github.com>
2022-11-09 12:15:37 +0100

    Update and rename Clase_Usuario.ts to Clase_Usuario.js

---
+++ Docs/Hito 1/Clase_Usuario.js
@@ -0,0 +1,20 @@
+class Usuario {
+    // Properties
+    _name: string;
+    _edad: number;
+    _correo: string;
+        
+    // Constructor
+    constructor(make: string, color: string, doors = 4) {
+    this._name = name;
+    this._edad = edad;
+    this._correo = correo;
+    
+    
+    // Accessors
+
+    
+    
+    // Methods
+
+}
