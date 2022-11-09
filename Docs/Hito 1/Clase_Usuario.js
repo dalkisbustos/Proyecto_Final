a02f4f35150ada411073ab12ccf9ec936d87b9d3
Dalkis Bustos Olivo <116747654+dalkisbustos@users.noreply.github.com>
2022-11-09 18:35:19 +0100

    Update Clase_Usuario.js

--- Docs/Hito 1/Clase_Usuario.js
+++ Docs/Hito 1/Clase_Usuario.js
@@ -1,26 +1,24 @@
 // Se declara una Clase Usuario
 class usuario {
   // Propiedades
-    _name: string;
-    _fecnac: number;
+    _nombre: string;
+    _fecnac: date;
     _correo: string;
 }
 
 // Se crean las instancias de los objetos basadas en la clase
-
 const medico = new usuario();
 const paciente= new usuario();
 const admor= new usuario();
-  
-        
-    // Constructor
-    constructor(name: string, fecnac: string, correo = 4) {
-    this._name = name;
-    this._edad = edad;
+         
+// Constructor
+    constructor(nombre: string, fecnac: string, correo = string) {
+    this._nombre = name;
+    this._fecnac = edad;
     this._correo = correo;
     
      
-    // Funciones
+// Funciones
 horariomedico(){
 console.log( "Horarios del Personal Médico");
 }
