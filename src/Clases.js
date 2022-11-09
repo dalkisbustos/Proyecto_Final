dd61209a2c8611a1f978624f54f65c44c1e73861
Dalkis Bustos Olivo <116747654+dalkisbustos@users.noreply.github.com>
2022-11-09 19:45:03 +0100

    Update Clases.js

--- src/Clases.js
+++ src/Clases.js
@@ -1,1 +1,31 @@
+// Se declara una Clase Usuario
+class usuario {
+  // Propiedades
+    _nombre: string;
+    _fecnac: date;
+    _correo: string;
+}
 
+// Se crean las instancias de los objetos basadas en la clase
+const medico = new usuario();
+const paciente= new usuario();
+const admor= new usuario();
+         
+// Constructor
+    constructor(nombre: string, fecnac: string, correo = string) {
+    this._nombre = name;
+    this._fecnac = edad;
+    this._correo = correo;
+    
+     
+// Funciones
+horariomedico(){
+console.log( "Horarios del Personal Médico");
+}
+        
+horariopaciente(){
+console.log("Horario Paciente");
+
+}
+        
+}
